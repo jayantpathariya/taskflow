2741869502a699fbab824a4702cc2bfd5171f6e5
jayantpathariya <jayantmehra02@gmail.com>
2026-09-11 21:10:46 +0530

    chore: setup project

---
+++ apps/server/src/app.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+
+app.get("/health", (_req, res) => {
+  res.json({
+    status: "ok",
+  });
+});
+
+export default app;
